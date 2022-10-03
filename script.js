f462d26ad874a9f053a1747f7a8246ec4a69fd60
Mirgubad <migubad@code.edu.az>
2022-10-03 16:38:28 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var github = document.querySelector(`.github`);
+var linkedIn = document.querySelector(`.linkedin`);
+
+github.addEventListener("click", () => {
+
+    window.open("https://github.com/Mirgubad")
+
+})
+
+linkedIn.addEventListener("click", () => {
+    window.open("https://www.linkedin.com/in/mirgubad-akbarov-191b32242")
+})
+
